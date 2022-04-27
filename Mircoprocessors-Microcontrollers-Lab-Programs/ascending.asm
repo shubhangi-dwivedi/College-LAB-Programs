d172c41aaf492fbed00b24b8cd8a63a37cf17445
Shubhangi Dwivedi <71788610+shubhangi-dwivedi@users.noreply.github.com>
2022-04-27 13:22:06 +0530

    arrange given numbers in ascending order

---
+++ Mircoprocessors-Microcontrollers-Lab-Programs/ascending.asm
@@ -0,0 +1,25 @@
+;arrange given numbers in ascending order
+
+data segment
+vec1 db 2,4,3,1,5 
+ends
+
+code segment
+start:mov dx,data
+mov ds,dx
+mov bx,4
+l1:mov cx,bx 
+lea si, vec1
+l2:mov al,[si]
+cmp al,[si+1]
+jc xx
+xchg al,[si+1]
+xchg al,[si]
+
+xx:inc si
+loop l2
+dec bx
+jnz l1
+hlt
+ends
+end start
